2758f1f99b6c12243d011158c02404d87e06f769
hyunah <qkrgusdk0303@naver.com>
2023-05-19 20:45:03 +0900

    Initialize project using Create React App

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+* {
+  box-sizing: border-box;
+}
+  a {
+  &:visited &:hover &:focus &:active {
+    text-decoration: none;
+  }
+}    
+body{
+  font-family: 'Lato', sans-serif; 
+  font-family: 'Noto Sans KR', sans-serif; 
+  }
+
+`;
+
+export default GlobalStyle;
