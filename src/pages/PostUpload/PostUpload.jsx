7a8c402c1ce1f5f93ce87b25707f4f940865aa3f
joyfuldev <wings2132@naver.com>
2023-05-22 18:33:24 +0900

    Modify: 불필요한 코드 삭제 및 사진 최대 3장까지 등록 할 수 있도록 수정

--- src/pages/PostUpload/PostUpload.jsx
+++ src/pages/PostUpload/PostUpload.jsx
@@ -1,19 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import ContentsBox from './components/ContentsBox';
+import variables from '../../styles/variables';
+
+const PostUpload = () => {
+  const [datas, setDatas] = useState([{ id: 1, img: '', desc: '' }]);
+
+  const handleAdd = event => {
+    if (datas.length > 2) return;
+    event.preventDefault();
+    setDatas(prev => [
+      ...prev,
+      { id: datas[datas.length - 1].id + 1, img: '', desc: '' },
+    ]);
+  };
+
+  return (
+    <Container>
+      <Form>
+        {datas.map(data => (
+          <ContentsBox
+            key={data.id}
+            data={data}
+            datas={datas}
+            setDatas={setDatas}
+            index={datas.length - 1}
+          />
+        ))}
+        {datas[datas.length - 1].img && datas.length <= 2 && (
+          <Add onClick={handleAdd}>
+            <span>추가하기</span>
+          </Add>
+        )}
+      </Form>
+    </Container>
+  );
+};
 
 const Container = styled.div``;
 
 const Form = styled.form``;
 
-const PostUpload = () => {
-  return (
-    <Container>
-      <Form>
-        <ContentsBox />
-      </Form>
-    </Container>
-  );
-};
+const Add = styled.button`
+  width: 950px;
+  margin: 0 auto;
+  height: 30px;
+  padding: 30px 0;
+  font-size: 17px;
+  ${variables.flex('row', 'center', 'center')}
+  border: none;
+  color: #858685;
+  font-weight: 600;
+  cursor: pointer;
+`;
 
 export default PostUpload;
